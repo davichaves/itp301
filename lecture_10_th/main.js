8aa3df2af2b137141594116da4af1c10e5a36979
Davi Chaves <dchaves@usc.edu>
2016-03-24 17:45:12 -0700

    working on lab7

---
+++ lecture_10_th/main.js
@@ -0,0 +1,27 @@
+var jsonObj = {
+	"firstName":"Tommy",
+	"lastName":"Trojan",
+	"age":21,
+	"phone":{
+		"cell":"123-123-1234",
+		"home":"789-789-7890"
+	},
+	"friends":[
+		{
+			"firstName":"John", 
+			"lastName":"Smith"
+		},
+		{
+			"firstName":"Jane", 
+			"lastName":"Doe"
+		}
+	]
+};
+
+
+var jsonStr = '{"firstName":"Tommy","lastName":"Trojan","age":21,"phone":{"cell":"123-123-1234","home":"789-789-7890"},"friends":[{"firstName":"John", "lastName":"Smith"},{"firstName":"Jane", "lastName":"Doe"}]}';
+
+
+/**********
+ * JSONP
+ **********/
